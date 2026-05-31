9bb9b4915bf4a09e227fc0a1f381115555aa0584
yutakobayashidev <hi@yutakobayashi.com>
2026-06-01 04:49:04 +0900

    refactor: convert DNS records to for_each pattern

--- infra/global/domains/yutakobayashi-com/main.tf
+++ infra/global/domains/yutakobayashi-com/main.tf
@@ -1,41 +1,50 @@
-# Mastodon DNS records (yutakobayashi.com zone)
-# Imported from existing Cloudflare records
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.10.1"
+    }
+  }
+}
 
-resource "cloudflare_dns_record" "mastodon_a" {
+locals {
   zone_id = var.cloudflare_zone_id
-  name    = "fedi"
-  type    = "A"
-  content = var.mastodon_ip
-  proxied = true
-  ttl     = 1
+  records = {
+    mastodon_a = {
+      content = var.mastodon_ip
+      name    = "fedi"
+      proxied = true
+      type    = "A"
+    }
+    mastodon_dkim_1 = {
+      content = "4epkx5liidlbwr24hvivm5hqdxkfvtfw.dkim.amazonses.com"
+      name    = "4epkx5liidlbwr24hvivm5hqdxkfvtfw._domainkey.fedi"
+      proxied = false
+      type    = "CNAME"
+    }
+    mastodon_dkim_2 = {
+      content = "5gualsmqbqr2iyy4jcswf45fnntumcec.dkim.amazonses.com"
+      name    = "5gualsmqbqr2iyy4jcswf45fnntumcec._domainkey.fedi"
+      proxied = false
+      type    = "CNAME"
+    }
+    mastodon_dkim_3 = {
+      content = "tvkogr4mjxbbpsortlpketewdt7rrlqb.dkim.amazonses.com"
+      name    = "tvkogr4mjxbbpsortlpketewdt7rrlqb._domainkey.fedi"
+      proxied = false
+      type    = "CNAME"
+    }
+  }
 }
 
 # fedi-files CNAME is auto-managed by R2 custom domain (module.mastodon_media)
 
-# SES DKIM records for fedi.yutakobayashi.com
-resource "cloudflare_dns_record" "mastodon_dkim_1" {
-  zone_id = var.cloudflare_zone_id
-  name    = "4epkx5liidlbwr24hvivm5hqdxkfvtfw._domainkey.fedi"
-  type    = "CNAME"
-  content = "4epkx5liidlbwr24hvivm5hqdxkfvtfw.dkim.amazonses.com"
-  proxied = false
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "mastodon_dkim_2" {
-  zone_id = var.cloudflare_zone_id
-  name    = "5gualsmqbqr2iyy4jcswf45fnntumcec._domainkey.fedi"
-  type    = "CNAME"
-  content = "5gualsmqbqr2iyy4jcswf45fnntumcec.dkim.amazonses.com"
-  proxied = false
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "mastodon_dkim_3" {
-  zone_id = var.cloudflare_zone_id
-  name    = "tvkogr4mjxbbpsortlpketewdt7rrlqb._domainkey.fedi"
-  type    = "CNAME"
-  content = "tvkogr4mjxbbpsortlpketewdt7rrlqb.dkim.amazonses.com"
-  proxied = false
-  ttl     = 1
+resource "cloudflare_dns_record" "record" {
+  for_each = local.records
+  zone_id  = local.zone_id
+  content  = each.value.content
+  name     = each.value.name
+  proxied  = each.value.proxied
+  ttl      = 1
+  type     = each.value.type
 }
