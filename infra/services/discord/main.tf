cda4e5ccda3fe76a4d219da76e3a91a5f5607adc
yutakobayashidev <hi@yutakobayashi.com>
2026-06-04 05:32:19 +0900

    feat: add discord workspace scaffold

---
+++ infra/services/discord/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.6"
+
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "yutakobayashi"
+    workspaces {
+      name = "discord"
+    }
+  }
+
+  required_providers {
+    discord = {
+      source  = "Lucky3028/discord"
+      version = "~> 2.7"
+    }
+  }
+}
+
+provider "discord" {
+  token = var.discord_token
+}
---
+++ infra/services/discord/variables.tf
@@ -0,0 +1,5 @@
+variable "discord_token" {
+  description = "Discord bot token"
+  type        = string
+  sensitive   = true
+}
