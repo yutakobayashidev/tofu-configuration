9b01c30531d65f6d3608706d8a6ce5f0be26f0d9
yutakobayashidev <hi@yutakobayashi.com>
2026-06-04 10:33:24 +0900

    feat: add member_roles and role resources to manage Discord roles via Terraform

---
+++ infra/services/discord/member_roles.tf
@@ -0,0 +1,504 @@
+resource "discord_member_roles" "nightbot" {
+  user_id   = "83010416610906112"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "979739714737274913"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "dyno" {
+  user_id   = "155149108183695360"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "906187018499948545"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "mee6" {
+  user_id   = "159985870458322944"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "906205605725863967"
+  }
+}
+
+resource "discord_member_roles" "carl_bot" {
+  user_id   = "235148962103951360"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "1205410901889654786"
+  }
+}
+
+resource "discord_member_roles" "owo" {
+  user_id   = "289066747443675143"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "961570032071081987"
+  }
+}
+
+resource "discord_member_roles" "reminder_bot" {
+  user_id   = "349920059549941761"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "973224742175768619"
+  }
+}
+
+resource "discord_member_roles" "zapier" {
+  user_id   = "368105370532577280"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "897817394486059028"
+  }
+}
+
+resource "discord_member_roles" "jockie_music" {
+  user_id   = "411916947773587456"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "955070893011382306"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "member_count" {
+  user_id   = "432533456807919639"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "920239776412610562"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "integromat" {
+  user_id   = "494177474192736270"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "928836173772763177"
+  }
+}
+
+resource "discord_member_roles" "shovel_blue" {
+  user_id   = "533698325203910668"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "919236762117746699"
+  }
+}
+
+resource "discord_member_roles" "member" {
+  user_id   = "712216055929372813"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.blender.id
+  }
+}
+
+resource "discord_member_roles" "tweetshift" {
+  user_id   = "713026372142104687"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "1016594320398233693"
+  }
+}
+
+resource "discord_member_roles" "t_人" {
+  user_id   = "723676173112180756"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "auto_publisher" {
+  user_id   = "739823232651100180"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "1043855499113807872"
+  }
+}
+
+resource "discord_member_roles" "statuspage" {
+  user_id   = "778802790843678740"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "928579986015793184"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "message_scheduler" {
+  user_id   = "810918366045798451"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "954530975864483873"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "activities" {
+  user_id   = "819778342818414632"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "yuta" {
+  user_id   = "890593691440398456"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.api_dev.id
+  }
+
+  role {
+    role_id = "1052496240950128710"
+  }
+
+  role {
+    role_id = discord_role.blueflow.id
+  }
+
+  role {
+    role_id = discord_role.admin.id
+  }
+
+  role {
+    role_id = discord_role.project_x.id
+  }
+}
+
+resource "discord_member_roles" "yuta_1" {
+  user_id   = "890908900520505354"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.admin.id
+  }
+}
+
+resource "discord_member_roles" "yukiya105" {
+  user_id   = "897380740176687107"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.project_x.id
+  }
+}
+
+resource "discord_member_roles" "yuta_studio" {
+  user_id   = "910521632970375178"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.develop.id
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = discord_role.admin.id
+  }
+
+  role {
+    role_id = "966495956587642974"
+  }
+}
+
+resource "discord_member_roles" "sakiyuki" {
+  user_id   = "915849554291736658"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.admin.id
+  }
+}
+
+resource "discord_member_roles" "price_tracker" {
+  user_id   = "921393343144017941"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "921717650235740172"
+  }
+}
+
+resource "discord_member_roles" "minecraft_changelog" {
+  user_id   = "924085094732406784"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.develop.id
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "928593122588499989"
+  }
+}
+
+resource "discord_member_roles" "midjourney_bot" {
+  user_id   = "936929561302675456"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "1094647868087861299"
+  }
+}
+
+resource "discord_member_roles" "splatoon" {
+  user_id   = "941287351894827029"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.develop.id
+  }
+
+  role {
+    role_id = "941744733405995010"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "notion" {
+  user_id   = "942758349089419315"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.develop.id
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "966527907042316301"
+  }
+}
+
+resource "discord_member_roles" "bot_sample" {
+  user_id   = "947872812146769930"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.develop.id
+  }
+
+  role {
+    role_id = "947892241089835040"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "height" {
+  user_id   = "955780901583880232"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "995974180820496427"
+  }
+}
+
+resource "discord_member_roles" "sandbox" {
+  user_id   = "961915679848673301"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "961916012977061921"
+  }
+}
+
+resource "discord_member_roles" "keep_voice_channel" {
+  user_id   = "973568297478017134"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.develop.id
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "yagoo" {
+  user_id   = "999210033701199972"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "999210302405083171"
+  }
+}
+
+resource "discord_member_roles" "ai_playground" {
+  user_id   = "1027217440523964427"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.bot.id
+  }
+
+  role {
+    role_id = "1027405909070200925"
+  }
+}
+
+resource "discord_member_roles" "data_analytics" {
+  user_id   = "1049967336259600394"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "1049985263536971808"
+  }
+
+  role {
+    role_id = discord_role.admin.id
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "vr用" {
+  user_id   = "1060812365475627028"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.admin.id
+  }
+}
+
+resource "discord_member_roles" "ai要約" {
+  user_id   = "1096976477943242834"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "1096977119365574776"
+  }
+
+  role {
+    role_id = discord_role.bot.id
+  }
+}
+
+resource "discord_member_roles" "文章_敬語変換機能_提供_電子計算機" {
+  user_id   = "1163747539787468891"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "1163750396494360646"
+  }
+}
+
+resource "discord_member_roles" "fix_twitter_opengraph" {
+  user_id   = "1174659737153982535"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = discord_role.admin.id
+  }
+
+  role {
+    role_id = "1175000700464406602"
+  }
+}
+
+resource "discord_member_roles" "claude" {
+  user_id   = "1394873980376322108"
+  server_id = "895564066922328094"
+
+  role {
+    role_id = "1394874687971922055"
+  }
+}
+
