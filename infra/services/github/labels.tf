e6858856f14aec04121cf751af9c630ec41f2314
yutakobayashidev <hi@yutakobayashi.com>
2026-06-18 21:47:00 +0900

    feat(github): manage dotnix safe-to-apply label

--- infra/services/github/labels.tf
+++ infra/services/github/labels.tf
@@ -4,3 +4,10 @@ resource "github_issue_label" "dotnix_safe_to_plan" {
   color       = "0e8a16"
   description = "Allow an OpenTofu plan to run for a reviewed pull request"
 }
+
+resource "github_issue_label" "dotnix_safe_to_apply" {
+  repository  = github_repository.dotnix.name
+  name        = "safe-to-apply"
+  color       = "b60205"
+  description = "Allow OpenTofu to apply a reviewed pull request"
+}
